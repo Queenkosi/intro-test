d2cc284e02173b22b4d5def95eb354cefc443320
Queenkosi <130911572+Queenkosi@users.noreply.github.com>
2024-04-01 10:26:43 -1200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,51 @@
+//function greet() {
+   // alert('Welcome to my website');
+//}
+
+
+//const greeting = () => {
+    //console.log('I hope you have a nice time using my website');
+//};
+
+//greeting();
+
+//String
+//Number
+//Array
+//Boolean
+//Object
+
+//let locate = 'Location';
+//let age = 50;
+//let hasEaten = false;
+
+//let items = [25, 45, 'food', 'abuja', [12, 'laptop'], true];
+
+//let student = {
+    //firstname: 'Evelyn',
+   //age: 70,
+    //state: 'USA',
+//};
+
+//console.log(student.firstname);
+
+//console.log(student['age']);
+
+//console.log(student.state);
+//console.log(items[4][0]);
+//console.log(hasEaten);
+
+let buttonOnPage = document.getElementById('btn');
+let bodyElement = document.getElementsByTagName('body');
+
+buttonOnPage.addEventListener('click', greetUser);
+let arr =['green', 'red', 'yellow', 'pink', 'blue'];
+for (let i =0; i++; i < arr.length){
+
+};
+arr.map((item)=>);
+
+function greetUser() {
+    //console.log('Welcome my esteemed user');
+    document.body.style.backgroundColor = 'red';
+}
---
+++ src/task.js
@@ -0,0 +1,27 @@
+let buttonOnPage = document.getElementById('btn');
+let bodyElement = document.getElementsByTagName('body');
+
+buttonOnPage.addEventListener('click', clickme);
+let arr =['green', 'red', 'yellow', 'pink', 'blue', 'brown']
+
+function generateColor() {
+    let color = '#';
+}
+for (let i =0; i++; i < arr.length){
+console.log(color);
+}
+
+arr.map((item) => {
+    const button = document.createElement('button');
+    button.style.backgroundColor = (changebgColor);
+    button.addEventListener ('click', () => {
+    });
+}
+);
+colorOptions.appendChild(button);
+function clickme() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'blue', 'yellow';
+    
+}
